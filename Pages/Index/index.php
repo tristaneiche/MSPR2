ef6306789eac25effe16908b3f458be3d363a6de
775pl <selma.eljabri1@gmail.com>
2022-04-21 19:06:13 +0200

    Sécurisation de A2F

--- Pages/Index/index.php
+++ Pages/Index/index.php
@@ -23,15 +23,35 @@ echo 'Désolé, vous devez être identifié pour accéder à cette page.';
 if(isset($_POST['retour'])){
     header("Location: " . '../connexion.php', true, 301);
 }
-} else { // si tu es bien connecté.
+} else { 
 ?>
     <h1>Bienvenue <?php echo $_SESSION['pseudo']; ?>!</h1>
 
     <p>Contenu sensible</p>
     <p class="message"><a href="../Deconnexion/logout.php">Déconnexion</a></p>
 <?php
+    require_once('../DetectIp/detectIp.php');
+    $detect_ip = new detectIp();
+    $ip = $detect_ip->detect_ip();
+    $details = file_get_contents("https://ipinfo.io/.$ip.?token=e9eb8ad2a16715");
+    $json = json_decode($details);
+
+    $ip = $json->ip;
+    $city = $json->city;
+    $postal = $json->postal;
+    $region = $json->region;
+    $country = $json->country;
+    $timezone = $json->timezone;
 }
 ?>
+    </div>
+    <div class="ip">
+        <p><?php echo "IP : " . $ip; ?></p>
+        <p><?php echo "Ville : " . $city; ?></p>
+        <p><?php echo "Code Postal : " . $postal; ?></p>
+        <p><?php echo "Région : " . $region; ?></p>
+        <p><?php echo "Pays : " . $country; ?></p>
+        <p><?php echo "Timezone : " . $timezone; ?></p>
     </div>
 </body>
 </html>
--- Pages/connexion.php
+++ Pages/connexion.php
@@ -17,6 +17,7 @@
             <input class="button" type="submit" name="submit" value="Connexion" />
             <p id="script" style="display: none; color:red; font-size: 12px;">Trop de tentatives d'authentification aujourd'hui. Revenez demain</p>
             <p id="ip" style="display: none; color:red; font-size: 12px;">Votre connexion provient d’un pays étranger, la connexion est bloquée</p>
+            <p id="browser" style="display: none; color:red; font-size: 12px;">Votre connexion provient d’un nouveau navigateur, veuillez confirmer votre identité par mail</p>
           </form>
         <p class="message">Un problème? <a href="mailto:lechatelet52@gmail.com">Contactez l'administrateur</a></p>
     </div>
@@ -119,92 +120,97 @@ if(isset($_POST['submit'])){
                 }elseif($resultat === TRUE){
                     //contenu 
                     echo "Connected";
-                    die;
-                
-
-                require_once('DetectBrowser/detectBrowser.php');
-                $detect_browser = new detectBrowser();
-                $browser = $detect_browser->detect_browser();
-                if($data_verif['navigateur'] == $browser){
-                    require_once('DetectIp/detectIp.php');
-                    $detect_ip = new detectIp();
-                    $ip = $detect_ip->detect_ip();
-                    $details = file_get_contents("https://ipinfo.io/.$ip.?token=e9eb8ad2a16715");
-                    $json = json_decode($details);
-                    $country = $json->country;
-                    
-                    if($country == "FR"){
-                        $_SESSION['pseudo'] = $data_verif['pseudo'];  
-                        header("Location: " . 'A2F/index.php', true, 301);
-                    }else{
-                                    $dest = ($data_verif['email']);
-                                    $objet="Mauvaise IP";
-                                    $message="Madame, Monsieur,
-Suite à une récente connexion sur votre compte Le Chatelet, nous avons constaté une activité suspecte. La connexion s'est opérée depuis un nouveau navigateur.";
-                                    $entetes="From: selma.eljabri@epsi.fr";
-                                    mail($dest, $objet, $message, $entetes);
-                                ?>
-                                    <script>
-                                    var x = document.getElementById("ip");
-                                    if (x.style.display === "none") {
-                                    x.style.display = "block";
-                                    } else {
-                                    x.style.display = "none";
-                                    }
-                                    </script>   <?php
-                            }
+                    require_once('DetectBrowser/detectBrowser.php');
+                    $detect_browser = new detectBrowser();
+                    $browser = $detect_browser->detect_browser();
+                    if($data_verif['navigateur'] == $browser){
+                        require_once('DetectIp/detectIp.php');
+                        $detect_ip = new detectIp();
+                        $ip = $detect_ip->detect_ip();
+                        $details = file_get_contents("https://ipinfo.io/.$ip.?token=e9eb8ad2a16715");
+                        $json = json_decode($details);
+                        $country = $json->country;
+                        
+                        if($country == "FR"){
+                            $_SESSION['pseudo'] = $data_verif['pseudo'];  
+                            header("Location: " . 'A2F/index.php', true, 301);
                         }else{
-                            echo "pas bon navigateur";
-                                $pseudo = $_POST['pseudo'];
-                                include_once 'Double_Co_Mail/randomizer.php';
-                                $random = new randomizer();
-                                $key = $random->str_random(40);
-            
-                                $dbh = new PDO('mysql:host=localhost;dbname=mspr', 'root', '');
-            
-                                $user_id = $data_verif['id'];
-                                $bdd = $dbh->prepare("UPDATE user SET key_confirm=:key_confirm WHERE pseudo like :pseudo");
-                                $bdd->bindParam(':key_confirm', $key);
-                                $bdd->bindParam(':pseudo', $pseudo);
-                                $bdd->execute();
-            
-                                $bdd2 = $dbh->prepare("UPDATE user SET confirmed=1 WHERE pseudo like :pseudo");
-                                $bdd2->bindParam(':pseudo', $pseudo);
-                                $bdd2->execute();
-                                $_SESSION['email'] = $data_verif['email'];
-                                $dest = ($_SESSION['email']);
-                                $objet="Mauvais navigateur";
-                                $message='Afin de valider votre compte merci de cliquer sur ce lien https://therealchatelet.fr?pseudo='.urlencode($pseudo).'&key_confirm='.urlencode($key);
-                                $entetes="MIME-Version: 1.0\r\nContent-type:text/html;charset=iso-8859-1\r\n";
-                                $entetes.="From: selma.eljabri@epsi.fr";
-                                mail($dest, $objet, $message, $entetes);
-            
-                                if(isset($_GET['key_confirm'])){
-                                    $keys = $_GET['key_confirm'];
-                                    $req = mysqli_query($mysqli, 'SELECT * FROM user WHERE id = '.$user_id);
-                                        if($req->execute(array(':pseudo'=> $pseudo)) && $row = $req->fetch()){
-                                            $keybdd = $row['key_confirm'];
-                                            $confirmed = $row['confirmed'];
+                                        $dest = ($data_verif['email']);
+                                        $objet="Mauvaise IP";
+                                        $message="Madame, Monsieur,
+    Suite à une récente connexion sur votre compte Le Chatelet, nous avons constaté une activité suspecte. La connexion s'est opérée depuis un nouveau navigateur.";
+                                        $entetes="From: selma.eljabri@epsi.fr";
+                                        mail($dest, $objet, $message, $entetes);
+                                    ?>
+                                        <script>
+                                        var x = document.getElementById("ip");
+                                        if (x.style.display === "none") {
+                                        x.style.display = "block";
+                                        } else {
+                                        x.style.display = "none";
                                         }
-                                        if($confirmed == '1'){
-                                            echo "compte deja actif";
-                                        }else{
-                                            if ($keys = $keybdd){
-                                                echo "compte activé";
-                                                header("Location: Index/index.php");
-                                            }else{
-                                                echo "erreur";
-                                            }
-                                        }
-                                    if(mail($dest, $objet, $message, $entetes)){
-                                        echo "mail ok";
-                                    }else{
-                                        echo "pas ok";
-                                    }
-                                    exit();
+                                        </script>   <?php
                                 }
-                        } 
-                        ldap_close($connect);  
+                            }else{
+                                    $pseudo = $_POST['pseudo'];
+                                    include_once 'Double_Co_Mail/randomizer.php';
+                                    $random = new randomizer();
+                                    $key = $random->str_random(40);
+                
+                                    $dbh = new PDO('mysql:host=localhost;dbname=mspr', 'root', '');
+                
+                                    $user_id = $data_verif['id'];
+                                    $bdd = $dbh->prepare("UPDATE user SET key_confirm=:key_confirm WHERE pseudo like :pseudo");
+                                    $bdd->bindParam(':key_confirm', $key);
+                                    $bdd->bindParam(':pseudo', $pseudo);
+                                    $bdd->execute();
+                
+                                    $bdd2 = $dbh->prepare("UPDATE user SET confirmed=1 WHERE pseudo like :pseudo");
+                                    $bdd2->bindParam(':pseudo', $pseudo);
+                                    $bdd2->execute();
+                                    $_SESSION['email'] = $data_verif['email'];
+                                    $dest = ($_SESSION['email']);
+                                    $objet="Mauvais navigateur";
+                                    $message='Afin de valider votre compte merci de cliquer sur ce lien https://therealchatelet.fr?pseudo='.urlencode($pseudo).'&key_confirm='.urlencode($key);
+                                    $entetes="MIME-Version: 1.0\r\nContent-type:text/html;charset=iso-8859-1\r\n";
+                                    $entetes.="From: selma.eljabri@epsi.fr";
+                                    mail($dest, $objet, $message, $entetes);
+                                    ?>
+                                        <script>
+                                        var x = document.getElementById("browser");
+                                        if (x.style.display === "none") {
+                                        x.style.display = "block";
+                                        } else {
+                                        x.style.display = "none";
+                                        }
+                                        </script>   <?php
+                
+                                    if(isset($_GET['key_confirm'])){
+                                        $keys = $_GET['key_confirm'];
+                                        $req = mysqli_query($mysqli, 'SELECT * FROM user WHERE id = '.$user_id);
+                                            if($req->execute(array(':pseudo'=> $pseudo)) && $row = $req->fetch()){
+                                                $keybdd = $row['key_confirm'];
+                                                $confirmed = $row['confirmed'];
+                                            }
+                                            if($confirmed == '1'){
+                                                echo "compte deja actif";
+                                            }else{
+                                                if ($keys = $keybdd){
+                                                    echo "compte activé";
+                                                    header("Location: Index/index.php");
+                                                }else{
+                                                    echo "erreur";
+                                                }
+                                            }
+                                        if(mail($dest, $objet, $message, $entetes)){
+                                            echo "mail ok";
+                                        }else{
+                                            echo "pas ok";
+                                        }
+                                        exit();
+                                    }
+                            } 
+                            ldap_close($connect);  
                     }  
                 }else{
                     echo "bind qui bug";
